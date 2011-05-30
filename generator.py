2ba7f1b43d97a4288be2f2cbc688c320fd70d4e2
Mark Seaborn <mrs@mythic-beasts.com>
2011-05-30 20:08:08 +0000

    Generator: Add movw and movb

--- generator.py
+++ generator.py
@@ -13,6 +13,26 @@ regs32 = (
   (6, '%esi'),
   (7, '%edi'))
 
+regs16 = (
+  (0, '%ax'),
+  (1, '%cx'),
+  (2, '%dx'),
+  (3, '%bx'),
+  (4, '%sp'),
+  (5, '%bp'),
+  (6, '%si'),
+  (7, '%di'))
+
+regs8 = (
+  (0, '%al'),
+  (1, '%cl'),
+  (2, '%dl'),
+  (3, '%bl'),
+  (4, '%ah'),
+  (5, '%ch'),
+  (6, '%dh'),
+  (7, '%bh'))
+
 
 def Sib(mod):
   for index_reg, index_regname in regs32:
@@ -39,7 +59,7 @@ def Sib(mod):
         yield [Byte((scale << 6) | (index_reg << 3) | base_reg)] + extra2, desc
 
 
-def ModRM1():
+def ModRM1(arg_regs):
   yield (0, 5, ['XX'] * 4, 'VALUE32')
   for mod, dispsize, disp_str in ((0, 0, ''),
                                   (1, 1, 'VALUE8'),
@@ -58,30 +78,40 @@ def ModRM1():
       yield (mod, reg2, sib_bytes + ['XX'] * dispsize,
              disp_str + desc)
   mod = 3
-  for reg2, regname2 in regs32:
+  for reg2, regname2 in arg_regs:
     yield (mod, reg2, [], regname2)
 
 
-def ModRM():
-  for reg, regname in regs32:
-    for mod, reg2, rest, desc in ModRM1():
+def ModRM(arg_regs):
+  for reg, regname in arg_regs:
+    for mod, reg2, rest, desc in ModRM1(arg_regs):
       yield ([Byte((mod << 6) | (reg << 3) | reg2)] + rest, regname, desc)
 
 
-def Mov():
-  for rest, op1, op2 in ModRM():
+def Mov1(arg_regs, arg_size):
+  for rest, op1, op2 in ModRM(arg_regs):
     # MOV r/m32,r32
-    yield [Byte(0x89)] + rest, 'movl %s, %s' % (op1, op2)
+    yield 0x89, 0x88, rest, '%s, %s' % (op1, op2)
     # MOV r32,r/m32
-    yield [Byte(0x8b)] + rest, 'movl %s, %s' % (op2, op1)
+    yield 0x8b, 0x8a, rest, '%s, %s' % (op2, op1)
     # MOV r/m32,imm32
-    yield [Byte(0xc7)] + rest + ['XX'] * 4, \
-        'movl $VALUE32, %s' % op2
-  yield [Byte(0xa1)] + ['XX'] * 4, 'movl VALUE32, %eax'
-  yield [Byte(0xa3)] + ['XX'] * 4, 'movl %eax, VALUE32'
+    yield 0xc7, 0xc6, rest + ['XX'] * arg_size, \
+        '$VALUE%i, %s' % (arg_size*8, op2)
+  yield 0xa1, 0xa0, ['XX'] * 4, 'VALUE32, %s' % arg_regs[0][1]
+  yield 0xa3, 0xa2, ['XX'] * 4, '%s, VALUE32' % arg_regs[0][1]
   # MOV reg32,imm32
-  for reg, regname in regs32:
-    yield [Byte(0xb8 + reg)] + ['XX'] * 4, 'movl $VALUE32, %s' % regname
+  for reg, regname in arg_regs:
+    yield 0xb8 + reg, 0xb0 + reg, ['XX'] * arg_size, \
+        '$VALUE%i, %s' % (arg_size*8, regname)
+
+
+def Mov():
+  for opcode_lw, opcode_b, bytes, desc in Mov1(regs32, 4):
+    yield [Byte(opcode_lw)] + bytes, 'movl ' + desc
+  for opcode_lw, opcode_b, bytes, desc in Mov1(regs16, 2):
+    yield [Byte(0x66), Byte(opcode_lw)] + bytes, 'movw ' + desc
+  for opcode_lw, opcode_b, bytes, desc in Mov1(regs8, 1):
+    yield [Byte(opcode_b)] + bytes, 'movb ' + desc
 
 
 seen = set()
