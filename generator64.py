47c42616f201895044237484f532733dc5301f18
Mark Seaborn <mrs@mythic-beasts.com>
2011-06-02 04:29:26 +0000

    Extend to handle top-bit-set registers

--- generator64.py
+++ generator64.py
@@ -17,7 +17,15 @@ regs64 = (
   (4, '%rsp'),
   (5, '%rbp'),
   (6, '%rsi'),
-  (7, '%rdi'))
+  (7, '%rdi'),
+  (8, '%r8'),
+  (9, '%r9'),
+  (10, '%r10'),
+  (11, '%r11'),
+  (12, '%r12'),
+  (13, '%r13'),
+  (14, '%r14'),
+  (15, '%r15'))
 
 regs32 = (
   (0, '%eax'),
@@ -27,7 +35,15 @@ regs32 = (
   (4, '%esp'),
   (5, '%ebp'),
   (6, '%esi'),
-  (7, '%edi'))
+  (7, '%edi'),
+  (8, '%r8d'),
+  (9, '%r9d'),
+  (10, '%r10d'),
+  (11, '%r11d'),
+  (12, '%r12d'),
+  (13, '%r13d'),
+  (14, '%r14d'),
+  (15, '%r15d'))
 
 regs16 = (
   (0, '%ax'),
@@ -37,9 +53,19 @@ regs16 = (
   (4, '%sp'),
   (5, '%bp'),
   (6, '%si'),
-  (7, '%di'))
+  (7, '%di'),
+  (8, '%r8w'),
+  (9, '%r9w'),
+  (10, '%r10w'),
+  (11, '%r11w'),
+  (12, '%r12w'),
+  (13, '%r13w'),
+  (14, '%r14w'),
+  (15, '%r15w'))
 
-regs8 = (
+# 8-bit registers accessible with no REX prefix.
+# These can be the low or high 8 bits of a 16-bit register.
+regs8_original = (
   (0, '%al'),
   (1, '%cl'),
   (2, '%dl'),
@@ -49,8 +75,37 @@ regs8 = (
   (6, '%dh'),
   (7, '%bh'))
 
+# 8-bit registers accessible with a REX prefix.
+# These are always the low 8 bits of a larger register.
+regs8_extended = (
+  (0, '%al'),
+  (1, '%cl'),
+  (2, '%dl'),
+  (3, '%bl'),
+  (4, '%spl'),
+  (5, '%bpl'),
+  (6, '%sil'),
+  (7, '%dil'),
+  (8, '%r8b'),
+  (9, '%r9b'),
+  (10, '%r10b'),
+  (11, '%r11b'),
+  (12, '%r12b'),
+  (13, '%r13b'),
+  (14, '%r14b'),
+  (15, '%r15b'))
+
 mem_regs = regs64
 zero_reg = '%riz'
+
+
+def RegTop(reg):
+  assert 0 <= reg and reg < 16
+  return (reg >> 3) & 1
+
+def RegBottom(reg):
+  assert 0 <= reg and reg < 16
+  return reg & 7
 
 
 def Sib(mod):
@@ -63,12 +118,12 @@ def Sib(mod):
       # 5 is a special case and is not always %ebp.
       # %esi/%edi are missing from headings in table in doc.
       for base_reg, base_regname in mem_regs:
-        # index_regname == '%eiz' and base_regname == '%esp'
-        if index_reg == 4 and base_reg == 4 and scale == 0:
+        # If index_regname == '%eiz' and base_regname == '%esp'
+        if index_reg == 4 and RegBottom(base_reg) == 4 and scale == 0:
           index_result = ''
         else:
           index_result = ', %s, %s' % (index_regname, 1 << scale)
-        if base_reg == 5 and mod == 0:
+        if RegBottom(base_reg) == 5 and mod == 0:
           base_regname = ''
           extra = 'VALUE32'
           extra2 = ['XX'] * 4
@@ -76,44 +131,52 @@ def Sib(mod):
           extra = ''
           extra2 = []
         desc = '%s(%s%s)' % (extra, base_regname, index_result)
-        # index_regname == '%eiz' and base_regname == ''.
-        if index_reg == 4 and base_reg == 5 and mod == 0 and scale == 0:
+        # If index_regname == '%eiz' and base_regname == ''.
+        if (index_reg == 4 and
+            RegBottom(base_reg) == 5 and mod == 0 and scale == 0):
           desc = extra
-        yield [Byte((scale << 6) | (index_reg << 3) | base_reg)] + extra2, desc
+        yield (RegTop(index_reg) << 1) | RegTop(base_reg), \
+            [Byte((scale << 6) |
+                  (RegBottom(index_reg) << 3) |
+                  RegBottom(base_reg))] \
+            + extra2, desc
 
 
+# yields (rex_bits, mod, reg, bytes, desc)
 def ModRM1(arg_regs):
-  yield (0, 5, ['XX'] * 4, 'VALUE32(%rip)')
+  yield (0, 0, 5, ['XX'] * 4, 'VALUE32(%rip)')
   for mod, dispsize, disp_str in ((0, 0, ''),
                                   (1, 1, 'VALUE8'),
                                   (2, 4, 'VALUE32')):
     for reg2, regname2 in mem_regs:
-      if reg2 == 4:
+      if RegBottom(reg2) == 4:
         # %esp is not accepted in this position.
         # 4 is a special value: adds SIB byte.
         continue
-      if reg2 == 5 and mod == 0:
+      if RegBottom(reg2) == 5 and mod == 0:
         continue
-      yield (mod, reg2, ['XX'] * dispsize,
+      yield (RegTop(reg2), mod, RegBottom(reg2), ['XX'] * dispsize,
              '%s(%s)' % (disp_str, regname2))
     reg2 = 4
-    for sib_bytes, desc in Sib(mod):
-      yield (mod, reg2, sib_bytes + ['XX'] * dispsize,
+    for rex_bits, sib_bytes, desc in Sib(mod):
+      yield (rex_bits, mod, reg2, sib_bytes + ['XX'] * dispsize,
              disp_str + desc)
   mod = 3
   for reg2, regname2 in arg_regs:
-    yield (mod, reg2, [], regname2)
+    yield (RegTop(reg2), mod, RegBottom(reg2), [], regname2)
 
 
 def ModRM(arg_regs):
   for reg, regname in arg_regs:
-    for mod, reg2, rest, desc in ModRM1(arg_regs):
-      yield ([Byte((mod << 6) | (reg << 3) | reg2)] + rest, regname, desc)
+    for rex_bits, mod, reg2, rest, desc in ModRM1(arg_regs):
+      yield ((RegTop(reg) << 2) | rex_bits,
+             [Byte((mod << 6) | (RegBottom(reg) << 3) | reg2)] + rest,
+             regname, desc)
 
 
 def ModRMSingleArg(arg_regs, opcode):
-  for mod, reg2, rest, desc in ModRM1(arg_regs):
-    yield ([Byte((mod << 6) | (opcode << 3) | reg2)] + rest, desc)
+  for rex_bits, mod, reg2, rest, desc in ModRM1(arg_regs):
+    yield (rex_bits, [Byte((mod << 6) | (opcode << 3) | reg2)] + rest, desc)
 
 
 patterns = (
@@ -137,28 +200,29 @@ def Generate1(arg_regs, arg_size, arg_size2):
   for opcode_lw, opcode_b, instr, args, modrm_opcode in patterns:
     if args == 'reg mem':
       assert modrm_opcode is None
-      for rest, op1, op2 in ModRM(arg_regs):
-        yield opcode_lw, opcode_b, rest, instr, '%s, %s' % (op1, op2)
+      for rex_bits, rest, op1, op2 in ModRM(arg_regs):
+        yield rex_bits, opcode_lw, opcode_b, rest, instr, '%s, %s' % (op1, op2)
     elif args == 'mem reg':
       assert modrm_opcode is None
-      for rest, op1, op2 in ModRM(arg_regs):
-        yield opcode_lw, opcode_b, rest, instr, '%s, %s' % (op2, op1)
+      for rex_bits, rest, op1, op2 in ModRM(arg_regs):
+        yield rex_bits, opcode_lw, opcode_b, rest, instr, '%s, %s' % (op2, op1)
     elif args == 'imm mem':
-      for rest, op in ModRMSingleArg(arg_regs, modrm_opcode):
-        yield opcode_lw, opcode_b, rest + ['XX'] * arg_size, \
+      for rex_bits, rest, op in ModRMSingleArg(arg_regs, modrm_opcode):
+        yield rex_bits, opcode_lw, opcode_b, rest + ['XX'] * arg_size, \
             instr, '$VALUE%i, %s' % (arg_size*8, op)
     elif args == 'imm8 mem':
-      for rest, op in ModRMSingleArg(arg_regs, modrm_opcode):
-        yield opcode_lw, opcode_b, rest + ['XX'], \
+      for rex_bits, rest, op in ModRMSingleArg(arg_regs, modrm_opcode):
+        yield rex_bits, opcode_lw, opcode_b, rest + ['XX'], \
             instr, '$VALUE8, %s' % op
     else:
       raise AssertionError('Unknown pattern: %r' % args)
 
-  yield 0xa1, 0xa0, ['XX'] * 8, 'mov', 'VALUE64, %s' % arg_regs[0][1]
-  yield 0xa3, 0xa2, ['XX'] * 8, 'mov', '%s, VALUE64' % arg_regs[0][1]
+  yield 0, 0xa1, 0xa0, ['XX'] * 8, 'mov', 'VALUE64, %s' % arg_regs[0][1]
+  yield 0, 0xa3, 0xa2, ['XX'] * 8, 'mov', '%s, VALUE64' % arg_regs[0][1]
   # MOV reg32,imm32
   for reg, regname in arg_regs:
-    yield 0xb8 + reg, 0xb0 + reg, ['XX'] * arg_size2, \
+    yield RegTop(reg), 0xb8 + RegBottom(reg), 0xb0 + RegBottom(reg), \
+        ['XX'] * arg_size2, \
         'mov', '$VALUE%i, %s' % (arg_size2*8, regname)
 
   # yield 0x05, 0x04, ['XX'] * arg_size, 'add', \
@@ -167,18 +231,33 @@ def Generate1(arg_regs, arg_size, arg_size2):
   #     '$VALUE%i, %s' % (arg_size*8, arg_regs[0][1])
 
 
+def Generate2():
+  for rex_bits, opcode_lw, opcode_b, bytes, instr, desc in \
+        Generate1(regs64, 4, 8):
+    yield (1 << 3) | rex_bits, \
+        [], [Byte(opcode_lw)] + bytes, instr + 'q ' + desc
+  for rex_bits, opcode_lw, opcode_b, bytes, instr, desc in \
+        Generate1(regs32, 4, 4):
+    yield rex_bits, [], [Byte(opcode_lw)] + bytes, instr + 'l ' + desc
+  for rex_bits, opcode_lw, opcode_b, bytes, instr, desc in \
+        Generate1(regs16, 2, 2):
+    yield rex_bits, [Byte(0x66)], [Byte(opcode_lw)] + bytes, instr + 'w ' + desc
+  for rex_bits, opcode_lw, opcode_b, bytes, instr, desc in \
+        Generate1(regs8_original, 1, 1):
+    if rex_bits == 0 and opcode_b is not None:
+      yield rex_bits, [], [Byte(opcode_b)] + bytes, instr + 'b ' + desc
+  for rex_bits, opcode_lw, opcode_b, bytes, instr, desc in \
+        Generate1(regs8_extended, 1, 1):
+    if rex_bits != 0 and opcode_b is not None:
+      yield rex_bits, [], [Byte(opcode_b)] + bytes, instr + 'b ' + desc
+
+
 def Generate():
-  for opcode_lw, opcode_b, bytes, instr, desc in Generate1(regs64, 4, 8):
-    reg_bits = 1 << 3
-    yield [Byte(0x40 + reg_bits),
-           Byte(opcode_lw)] + bytes, instr + 'q ' + desc
-  for opcode_lw, opcode_b, bytes, instr, desc in Generate1(regs32, 4, 4):
-    yield [Byte(opcode_lw)] + bytes, instr + 'l ' + desc
-  for opcode_lw, opcode_b, bytes, instr, desc in Generate1(regs16, 2, 2):
-    yield [Byte(0x66), Byte(opcode_lw)] + bytes, instr + 'w ' + desc
-  for opcode_lw, opcode_b, bytes, instr, desc in Generate1(regs8, 1, 1):
-    if opcode_b is not None:
-      yield [Byte(opcode_b)] + bytes, instr + 'b ' + desc
+  for rex_bits, size_prefix, bytes, desc in Generate2():
+    if rex_bits == 0:
+      yield size_prefix + bytes, desc
+    else:
+      yield size_prefix + [Byte(0x40 | rex_bits)] + bytes, desc
 
 
 seen = set()
@@ -270,7 +349,8 @@ def MapWildcard(byte):
 def DisassembleTest():
   fh = open('tmp.S', 'w')
   for bytes, desc in Generate():
-    asm = '.ascii "%s"\n' % ''.join('\\x' + MapWildcard(byte) for byte in bytes)
+    asm = '.ascii "%s" /* %s */\n' % (
+      ''.join('\\x' + MapWildcard(byte) for byte in bytes), desc)
     fh.write(asm)
   fh.close()
   subprocess.check_call(['gcc', '-c', m_bits, 'tmp.S', '-o', 'tmp.o'])
