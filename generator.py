12fb51cf5b418a47c20ac236b5a48bb7f0c09dff
Mark Seaborn <mrs@mythic-beasts.com>
2011-09-05 14:01:25 -0700

    DFA-gen: Add all the core arithmetic instructions
    Change testing to check just a subset of instructions.

--- generator.py
+++ generator.py
@@ -50,6 +50,30 @@ mem_sizes = {
   }
 
 
+def AssertEq(x, y):
+  if x != y:
+    raise AssertionError('%r != %r' % (x, y))
+
+
+def CatBits(values, sizes_in_bits):
+  result = 0
+  for value, size_in_bits in zip(values, sizes_in_bits):
+    assert isinstance(value, int)
+    assert 0 <= value
+    assert value < (1 << size_in_bits)
+    result = (result << size_in_bits) | value
+  return result
+
+
+def CatBitsRev(value, sizes_in_bits):
+  parts = []
+  for size_in_bits in reversed(sizes_in_bits):
+    parts.insert(0, value & ((1 << size_in_bits) - 1))
+    value >>= size_in_bits
+  AssertEq(value, 0)
+  return tuple(parts)
+
+
 def Sib(mod):
   for index_reg, index_regname in regs32:
     if index_reg == 4:
@@ -114,9 +138,10 @@ def ModRMSingleArg(arg_regs, opcode):
     yield ([Byte((mod << 6) | (opcode << 3) | reg2)] + rest, desc)
 
 
-def TrieNode(children):
+def TrieNode(children, accept=False):
   node = trie.Trie()
   node.children = children
+  node.accept = accept
   return node
 
 
@@ -137,6 +162,39 @@ def DftLabels(pairs, node):
   for key, value in pairs:
     node = DftLabel(key, value, node)
   return node
+
+
+# Assumes all the input nodes are immutable.
+def MergeMany(nodes, merge_accept_types):
+  if len(nodes) == 1:
+    return list(nodes)[0]
+  if len(nodes) == 0:
+    return EmptyNode
+  children = {}
+  accept_types = set()
+
+  if isinstance(nodes[0], DftLabel):
+    for node in nodes:
+      AssertEq(node.key, nodes[0].key)
+      AssertEq(node.value, nodes[0].value)
+    return DftLabel(nodes[0].key,
+                    nodes[0].value,
+                    MergeMany([node.next for node in nodes],
+                              merge_accept_types))
+
+  by_key = {}
+  for node in nodes:
+    accept_types.add(node.accept)
+    for key, value in node.children.iteritems():
+      by_key.setdefault(key, []).append(value)
+  for key, subnodes in by_key.iteritems():
+    children[key] = MergeMany(subnodes, merge_accept_types)
+
+  if len(accept_types) == 1:
+    accept = list(accept_types)[0]
+  else:
+    accept = merge_accept_types(accept_types)
+  return trie.MakeInterned(children, accept)
 
 
 from memoize import Memoize
@@ -173,17 +231,26 @@ def ModRMNode(reg_size, rm_size, immediate_size):
     nodes.append(TrieOfList(bytes,
                             DftLabels([('reg_arg', reg_arg),
                                        ('rm_arg', rm_arg)], tail)))
-  return trie.MergeMany(nodes, NoMerge)
+  node = MergeMany(nodes, NoMerge)
+  return TrieNode(dict((key, DftLabel('test_keep', key == '00', value))
+                       for key, value in node.children.iteritems()))
 
 
 @Memoize
-def ModRMSingleArgNode(rm_size, opcode, immediate_size):
+def ModRMSingleArgNode(rm_size, opcode, instr_name, immediate_size):
   nodes = []
   tail = TrieOfList(['XX'] * immediate_size, trie.AcceptNode)
   for bytes, rm_arg in ModRMSingleArg(rm_size, opcode):
     nodes.append(TrieOfList(bytes,
                             DftLabels([('rm_arg', rm_arg)], tail)))
-  return trie.MergeMany(nodes, NoMerge)
+  node = MergeMany(nodes, NoMerge)
+  def Filter(byte):
+    mod, reg1, reg2 = CatBitsRev(byte, [2, 3, 3])
+    return mod == 0 and reg2 == 0
+  return TrieNode(dict((key, DftLabel('test_keep', Filter(int(key, 16)),
+                                      DftLabel('instr_name', instr_name,
+                                               value)))
+                       for key, value in node.children.iteritems()))
 
 
 def FlattenTrie(node, bytes=[], labels=[]):
@@ -196,6 +263,22 @@ def FlattenTrie(node, bytes=[], labels=[]):
     for byte, next in sorted(node.children.iteritems()):
       for result in FlattenTrie(next, bytes + [byte], labels):
         yield result
+
+
+@Memoize
+def FilterModRM(node):
+  if isinstance(node, DftLabel):
+    if node.key == 'test_keep' and not node.value:
+      return trie.EmptyNode
+    return DftLabel(node.key, node.value, FilterModRM(node.next))
+  else:
+    children = {}
+    for key, value in node.children.iteritems():
+      value = FilterModRM(value)
+      if value != trie.EmptyNode:
+        children[key] = value
+    return TrieNode(children, node.accept)
+
 
 def GetRoot():
   top_nodes = []
@@ -211,37 +294,60 @@ def GetRoot():
       assert modrm_opcode is None
       node = ModRMNode(sizes[0], sizes[1], 0)
     elif parts == ['rm', 'imm']:
+      assert modrm_opcode is not None
       node = DftLabel('imm_arg', 'VALUE%i' % size,
-                      ModRMSingleArgNode(sizes[0], modrm_opcode, sizes[1] / 8))
+                      ModRMSingleArgNode(sizes[0], modrm_opcode, instr_name,
+                                         sizes[1] / 8))
     elif parts == ['*ax', 'imm']:
+      assert modrm_opcode is None
       node = DftLabels([('imm_arg', 'VALUE%i' % size),
                         ('*ax_arg', regs_by_size[size][0][1])],
                        TrieOfList(['XX'] * (sizes[1] / 8), trie.AcceptNode))
     else:
       raise AssertionError('Unknown pattern: %r' % args)
-    top_nodes.append(TrieOfList(bytes, DftLabels([('instr_name', instr_name),
-                                                  ('args', parts)],
-                                                 node)))
+    node = DftLabel('args', parts, node)
+    if modrm_opcode is None:
+      node = DftLabel('instr_name', instr_name, node)
+    top_nodes.append(TrieOfList(bytes, node))
 
-  Add('01', 'add', [('rm', 32), ('reg', 32)])
-  Add('03', 'add', [('reg', 32), ('rm', 32)])
-  Add('05', 'add', [('*ax', 32), ('imm', 32)])
-  Add('80', 'add', [('rm', 8), ('imm', 8)], modrm_opcode=0)
+  def AddPair(opcode, instr, format, **kwargs):
+    Add(Byte(opcode), instr, [(arg, 8) for arg in format], **kwargs)
+    Add('66 ' + Byte(opcode + 1), instr, [(arg, 16) for arg in format], **kwargs)
+    Add(Byte(opcode + 1), instr, [(arg, 32) for arg in format], **kwargs)
+
+  for arith_opcode, instr in enumerate(['add', 'or', 'adc', 'sbb',
+                                        'and', 'sub', 'xor', 'cmp']):
+    for format_num, format in enumerate([['rm', 'reg'],
+                                         ['reg', 'rm'],
+                                         ['*ax', 'imm']]):
+      opcode = CatBits([arith_opcode, format_num, 0], [5, 2, 1])
+      AddPair(opcode, instr, format)
+    AddPair(0x80, instr, ['rm', 'imm'], modrm_opcode=arith_opcode)
+    # 0x82 is a hole in the table...
+    Add('66 83', instr, [('rm', 16), ('imm', 8)], modrm_opcode=arith_opcode)
+    Add('83', instr, [('rm', 32), ('imm', 8)], modrm_opcode=arith_opcode)
+
   Add('0f b6', 'movzx', [('reg', 32), ('rm', 8)])
   Add('0f b7', 'movzx', [('reg', 32), ('rm', 16)])
   Add('0f be', 'movsx', [('reg', 32), ('rm', 8)])
   Add('0f bf', 'movsx', [('reg', 32), ('rm', 16)])
-  return trie.MergeMany(top_nodes, NoMerge)
+  return MergeMany(top_nodes, NoMerge)
 
-def GetAll():
-  for bytes, labels in FlattenTrie(GetRoot()):
+def InstrFromLabels(args):
+  instr_args = ', '.join([args['%s_arg' % arg] for arg in args['args']])
+  return '%s %s' % (args['instr_name'], instr_args)
+
+def GetAll(node):
+  for bytes, labels in FlattenTrie(node):
     args = dict((label.key, label.value) for label in labels)
-    i_args = ', '.join([args['%s_arg' % arg] for arg in args['args']])
-    instr = '%s %s' % (args['instr_name'], i_args)
-    yield (bytes, instr)
+    yield (bytes, InstrFromLabels(args))
 
 import objdump_check
 
-print TrieSize(GetRoot(), False)
-
-objdump_check.DisassembleTest(GetAll, bits=32)
+trie_root = GetRoot()
+print TrieSize(trie_root, False)
+print 'testing...'
+filtered_trie = FilterModRM(trie_root)
+for bytes, labels in GetAll(filtered_trie):
+  print '%s:%s' % (' '.join(bytes), labels)
+objdump_check.DisassembleTest(lambda: GetAll(filtered_trie), bits=32)
